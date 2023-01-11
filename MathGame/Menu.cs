9f0c4f934a8de6c11b7ba1bdc1d48733afd75b78
Sandis <smanssandis@gmail.com>
2023-01-11 03:12:50 +0200

    Introduction to the application on startup
    - User chooses available operand

---
+++ MathGame/Menu.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathGame
+{
+    internal class Menu
+    {
+        internal void StartGame()
+        {
+            bool gameInProcess = true;
+
+            do
+            {
+                Console.Clear();
+                PrintIntroduction();
+
+                string userInput = Console.ReadLine();
+
+                switch (userInput.Trim().ToUpper())
+                {
+                    case "A":
+                        Console.WriteLine("Addition");
+                        Console.ReadLine();
+                        break;
+                    case "S":
+                        Console.WriteLine("Subtraction");
+                        Console.ReadLine();
+                        break;
+                    case "M":
+                        Console.WriteLine("Multiplication");
+                        Console.ReadLine();
+                        break;
+                    case "D":
+                        Console.WriteLine("Division");
+                        Console.ReadLine();
+                        break;
+                    case "Q":
+                        Console.WriteLine("Goodbye!");
+                        gameInProcess = false;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Input");
+                        Console.ReadLine();
+                        break;
+                }
+            } while (gameInProcess);
+
+        }
+
+        private void PrintIntroduction()
+        {
+            Console.WriteLine("Welcome to the Math Game! Where your knowledge in mathematics will be tested");
+            Console.WriteLine("Choose from the options below which math operand you want to test your skills with:");
+            Console.WriteLine("-------------------------------------------");
+            Console.WriteLine("A - Addition");
+            Console.WriteLine("S - Subraction");
+            Console.WriteLine("M - Multiplication");
+            Console.WriteLine("D - Division");
+            Console.WriteLine("Q - Quit");
+            Console.WriteLine();
+        }
+    }
+}
--- MathGame/Program.cs
+++ MathGame/Program.cs
@@ -1,5 +1,17 @@
 ﻿// High level steps to create the project
 
 // 1) Write up a introduction to the game
+//  1) Create a new class called Menu 
+//  2) Menu class will have a introduction to the game with the available game options
+
+
+
+
 // 2) Get the user input to decide witch game to play
 // 3) Implement each operators method
+
+using MathGame;
+
+Menu mainGame = new Menu();
+
+mainGame.StartGame();
